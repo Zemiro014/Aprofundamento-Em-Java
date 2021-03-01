daf9e0fe3c29cdd2a890ca7945f5adb7ceb88938
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-03-01 13:48:47 -0300

    Adicionando a class Product com seus atributos e métodos

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,9 +2,7 @@ package application;
 
 import java.util.Locale;
 import java.util.Scanner;
-
-import entities.Calculate;
-import entities.Triangle;
+import entities.Product;
 
 public class Program {
 	
@@ -13,36 +11,19 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Triangle X, Y;
-		X = new Triangle();
-		Y = new Triangle();
+		Product product = new Product();
 		
-		System.out.println("Enter the measures of triangle X: ");
-		X.a = sc.nextDouble();
-		X.b = sc.nextDouble();
-		X.c = sc.nextDouble();
+		System.out.println("Enter product data: ");		
+		System.out.print("Name: ");
+		product.name = sc.nextLine();
+		System.out.print("Price: ");
+		product.price = sc.nextDouble();
+		System.out.print("Quantity: ");
+		product.quantity = sc.nextInt();
 		
-		System.out.println("Enter the measures of triangle X: ");
-		Y.a = sc.nextDouble();
-		Y.b = sc.nextDouble();
-		Y.c = sc.nextDouble();
+		System.out.println(product);		
 		
-		System.out.printf("Triangle X area: %.4f%n", X.area());
-		System.out.printf("Triangle Y area: %.4f%n", Y.area());
-		
-		Calculate calc = new Calculate();
-		
-		if (calc.comparingTheValueOf_X_And_Y(X.area(), Y.area()) == 1) 
-		{			
-			System.out.println("The X area is greater then Y area");			
-		}
-		else if (calc.comparingTheValueOf_X_And_Y(X.area(), Y.area()) == 2) 
-		{			
-			System.out.println("The areas have the same values");			
-		}
-		else {
-			System.out.println("The Y area is greater then X area");	
-		}
+		sc.close();
 	}
 
 }
