0434d9009784430a13701b8f50ceea0f69874006
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-03-05 17:11:26 -0300

    Lendo as informações de um arquivo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,58 +1,34 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
 import java.util.Scanner;
-
-import entities.Circle;
-import entities.Rectangle;
-import entities.Shape;
-import entities.enums.Color;
 
 public class Program {
 	
 	public static void  main(String[] args) throws ParseException {
 		
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		List<Shape> list = new ArrayList<>();
+		// Programa que lê as informações de um arquivo
 		
-		System.out.print("Entenr the number of shapes: ");
-		int n = sc.nextInt();
+		File file = new File("c:\\temp\\in.txt");		
+		Scanner sc = null;
 		
-		for(int i = 1; i <= n; i++) 
-		{
-			System.out.println("Shape #"+i +" data: ");
-			System.out.print("Rectangle or Circle (r/c)? : ");
-			char ch = sc.next().charAt(0);
-			System.out.print("Color (BLACK/BLUE/RED):");
-			String color = sc.next();
-			if(ch == 'r')
-			{
-				System.out.print("Width:");
-				double width = sc.nextDouble();
-				System.out.print("Height:");
-				double height = sc.nextDouble();
-				list.add(new Rectangle(Color.valueOf(color), width, height));
-			}
-			else if (ch == 'c') 
-			{
-				System.out.print("Radius:");
-				double radius = sc.nextDouble();
-				list.add(new Circle(Color.valueOf(color), radius));
+		try {
+			sc = new Scanner(file);
+			
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
 			}
 		}
-		
-		System.out.println();
-		System.out.println("SHAPE AREAS");
-		for(Shape sh : list) {
-			System.out.println(String.format("%.2f", sh.area()));
+		catch(IOException e) {
+			System.out.println("Error: "+e.getMessage());
 		}
-		
-		
-		sc.close();
+		finally {
+			if(sc != null) {
+				sc.close();
+			}
+		}
 	}
 
 }
