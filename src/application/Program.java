d3c990a6ceb035355d1f69721d1e7759fc3e2a16
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-03-05 18:10:30 -0300

    Lendo as informações de um arquivo usando Streams (FileReader e BufferedReader) em um bloco try-with-resources

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,16 +9,11 @@ public class Program {
 	
 	public static void  main(String[] args) throws ParseException {
 		
-		// Programa que lê as informações de um arquivo usando "Streams" (BufferedReader, FileReader)		
-		String path = "c:\\temp\\in.txt";		
+		// Programa que lê as informações de um arquivo usando "Streams" (BufferedReader, FileReader) em um bloco "try-with-resources"		
+		String path = "c:\\temp\\in.txt";
 		
-		FileReader fr = null;
-		BufferedReader br = null;
-		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-			
+		try(BufferedReader br = new BufferedReader(new FileReader(path)))
+		{
 			String line = br.readLine();
 			
 			while (line != null) {
@@ -29,22 +24,6 @@ public class Program {
 		catch(IOException e) 
 		{
 			System.out.println("Error: "+e.getMessage());			
-		}
-		finally {
-			try {
-				if(br != null) 
-				{
-					br.close();
-				}
-				if(fr != null) 
-				{
-					br.close();
-				}
-			}
-			catch(IOException e) 
-			{
-				e.printStackTrace();
-			}
 		}
 	}
 
