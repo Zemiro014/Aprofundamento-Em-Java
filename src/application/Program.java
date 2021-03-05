4ee99dd2ea1d5f979f2ebf5699ac0af3e69e2e28
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-03-05 18:56:55 -0300

    Criar um arquivo e escrever nele uma informaçaõ Usando BufferedWriter e FileWriter

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,7 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 
@@ -9,21 +9,29 @@ public class Program {
 	
 	public static void  main(String[] args) throws ParseException {
 		
-		// Programa que lê as informações de um arquivo usando "Streams" (BufferedReader, FileReader) em um bloco "try-with-resources"		
-		String path = "c:\\temp\\in.txt";
+		// Programa que escreve caracteres em um arquivo usando "Streams" (BufferedReader, FileReader) em um bloco "try-with-resources"	
 		
-		try(BufferedReader br = new BufferedReader(new FileReader(path)))
+		String[] lines = new String[] {"Good morning", "Good afternoon", "Good night", "Good evning"};
+		
+		String path = "c:\\temp\\out.txt";
+		
+		/*
+		 * Se vc não especificar o "true" em "FileWriter", cada vez que tu rodar o programa ele irá destruir o arquivo anterior e recria-lo. 
+		 * 
+		 * Com "true" especificado ele irá adicionar novos conteudos ao arquivo exixtente sempre que vc rodar o programa
+		 * */
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) 
 		{
-			String line = br.readLine();
 			
-			while (line != null) {
-				System.out.println(line);
-				line = br.readLine();
+			for (String line : lines) 
+			{
+				bw.write(line);
+				bw.newLine();
 			}
 		}
 		catch(IOException e) 
 		{
-			System.out.println("Error: "+e.getMessage());			
+			e.printStackTrace();			
 		}
 	}
 
