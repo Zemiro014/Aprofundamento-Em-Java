1ac4ffcebc8f0413270d0d0605e1b47695eff606
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-03-08 22:27:24 -0300

    Stack trace

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,13 +6,24 @@ import java.util.Scanner;
 
 public class Program {
 	
-	public static void  main(String[] args) throws ParseException {
-		
-		// Programa que pega as informações de um File
-		
-		Scanner sc = new Scanner(System.in);		
-		
-		try 
+	public static void  main(String[] args) throws ParseException 
+	{		
+		// Por padrão em java, quando O "EXCEPTION" não é tratada o programa termina de executar no ponto em que originou A "EXCEÇÃO".
+		 method1();		
+		System.out.println("End of program");	
+	}
+	
+	public static void method1() 
+	{
+		System.out.println("***Method1 start***");
+		method2();
+		System.out.println("***Method1 end***");
+	}
+	public static void method2()
+	{
+		System.out.println("***Method2 start***");
+		Scanner sc = new Scanner(System.in);
+		try
 		{
 			String[] vect = sc.nextLine().split(" ");
 			int position = sc.nextInt();
@@ -20,14 +31,14 @@ public class Program {
 		}
 		catch(ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid position !");
+			e.printStackTrace(); // serve para rastrear o percurso da excessão em questão
+			sc.nextLine();
 		}
 		catch(InputMismatchException e) {
 			System.out.println("Input Error");
 		}
 		
-		System.out.println("End of program");
-		
 		sc.close();
+		System.out.println("***Method2 end***");
 	}
-
 }
